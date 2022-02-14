86c918f1da0d9a9826086395c40e999e28a539e8
DustinSchouten <dustinschouten2@gmail.com>
2022-02-14 15:05:31 +0100

    api-werkend
    Data api in txt file toegevoegd als api crasht

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-document.querySelector('button').addEventListener('click',renderQuoteToHTML)
-// document.querySelector('button').addEventListener('click',generateQuote)
+document.querySelector('button').addEventListener('click',generateQuote)
 
 function generateQuote() {
   const apiURL = 'https://quote.api.fdnd.nl/v1/quote'
@@ -12,20 +11,18 @@ function generateQuote() {
     })
 }
 
-function renderQuoteToHTML() {
-  // const quotesLength = data.length
-  // const randomQuoteId = Math.floor(Math.random()*quotesLength);
-  // const quoteText = data[randomQuoteId].text
-  // const name = data[randomQuoteId].name
-  // const bio = data[randomQuoteId].bio
-  // const tags = data[randomQuoteId].tags
+function renderQuoteToHTML(data) {
+  const quotesLength = data.length
+  const randomQuoteId = Math.floor(Math.random()*quotesLength);
+  const quoteText = data[randomQuoteId].text
+  const name = data[randomQuoteId].name
+  const bio = data[randomQuoteId].bio
+  const tags = data[randomQuoteId].tags
+  document.querySelector('section').style.display = 'flex'
 
-  const name = 'Dustin Schouten' // remove later
-  const bio = 'The best man!!!' // remove later
-  const tags = '#tag tag' // remove later
 
-  // const avatarImgURL = data[randomQuoteId].avatar
-  // document.querySelector('#quoteText').textContent = randomQuoteId.toString() + quoteText
+  const avatarImgURL = data[randomQuoteId].avatar
+  document.querySelector('#quoteText').textContent = quoteText
   document.querySelector('#name').innerHTML = '<strong>Name: </strong>' + name
   document.querySelector('#bio').innerHTML = '<strong>Bio: </strong>' + bio
   document.querySelector('#tags').innerHTML = tags
@@ -34,7 +31,7 @@ function renderQuoteToHTML() {
   document.querySelector('#quoteText').style.animation = 'none';
   document.querySelector('#quoteText').offsetHeight;
   document.querySelector('#quoteText').style.animation = null;
-  // document.querySelector('#avatar').src = avatarImgURL
+  document.querySelector('#avatar').src = avatarImgURL
   if (quoteText.length > 100) {
     document.querySelector('#quoteText').style.fontSize = '1.5em';
   }
