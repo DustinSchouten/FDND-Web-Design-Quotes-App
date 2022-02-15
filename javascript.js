2760f37360c19e6a1c2ae8beea61a9f4c3e5dfff
DustinSchouten <dustinschouten2@gmail.com>
2022-02-15 14:33:23 +0100

    UI-States toegevoegd

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 document.querySelector('button').addEventListener('click',generateQuote)
 
 function generateQuote() {
+  showLoadingState()
+
   const apiURL = 'https://quote.api.fdnd.nl/v1/quote'
   fetch(apiURL)
-    .then(function(response) {
-        return response.json()
+    .then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      }
+      else {
+        throw Error(response.statusText);
+      }
     })
     .then(function(data) {
+      hideLoadingState()
+      const quotesLength = data.length
+      if (quotesLength == 0) {
+        showEmptyState()
+      }
+      else {
         renderQuoteToHTML(data.data)
+      }
     })
+    .catch((error) => {
+      showErrorState()
+    })
+    .finally(() => {
+      console.log('Program finished');
+    });
 }
 
 function renderQuoteToHTML(data) {
@@ -18,24 +38,48 @@ function renderQuoteToHTML(data) {
   const name = data[randomQuoteId].name
   const bio = data[randomQuoteId].bio
   const tags = data[randomQuoteId].tags
-  document.querySelector('section').style.display = 'flex'
-
-
   const avatarImgURL = data[randomQuoteId].avatar
   document.querySelector('#quoteText').textContent = quoteText
-  document.querySelector('#name').innerHTML = '<strong>Name: </strong>' + name
-  document.querySelector('#bio').innerHTML = '<strong>Bio: </strong>' + bio
+  document.querySelector('#name').innerHTML =  name
+  document.querySelector('#bio').innerHTML = bio
   document.querySelector('#tags').innerHTML = tags
+  document.querySelector('#personal_info_normal_state').style.display = 'flex'
 
   //Restart the animation
-  document.querySelector('#quoteText').style.animation = 'none';
-  document.querySelector('#quoteText').offsetHeight;
-  document.querySelector('#quoteText').style.animation = null;
-  document.querySelector('#avatar').src = avatarImgURL
-  if (quoteText.length > 100) {
-    document.querySelector('#quoteText').style.fontSize = '1.5em';
+  document.querySelector('#quoteText').style.animation = 'none'
+  document.querySelector('#quoteText').offsetHeight
+  document.querySelector('#quoteText').style.animation = null
+  if (avatarImgURL != '') { // If there is no image
+    document.querySelector('#avatar').src = avatarImgURL
   }
   else {
-    document.querySelector('#quoteText').style.fontSize = '2em';
+    document.querySelector('#avatar').src = 'image_skeleton.png'
   }
+
+  if (quoteText.length > 100) { // Set the font size smaller if the quote has much text
+    document.querySelector('#quoteText').style.fontSize = '1.5em'
+  }
+  else {
+    document.querySelector('#quoteText').style.fontSize = '2em'
+  }
+}
+
+function showLoadingState() {
+  document.querySelector('#textballoon').style.display = 'none'
+  document.querySelector('#textballoon_loading_state').style.display = 'flex'
+  document.querySelector('#personal_info_loading_state').style.display = 'flex'
+}
+
+function hideLoadingState() {
+  document.querySelector('#textballoon').style.display = 'flex'
+  document.querySelector('#textballoon_loading_state').style.display = 'none'
+  document.querySelector('#personal_info_loading_state').style.display = 'none'
+}
+
+function showEmptyState() {
+  document.querySelector('#empty_state').style.display = 'inherit'
+}
+
+function showErrorState() {
+  document.querySelector('#error_state').style.display = 'inherit'
 }
